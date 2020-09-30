19f1b7729755210ba1c31b328dfd097c956e84c9
Ayush _Gupta <31875557+Aa-yush@users.noreply.github.com>
2020-10-01 01:08:43 +0530

    Add files via upload

---
+++ Pattern_6.java
@@ -0,0 +1,19 @@
+
+public class Pattern_6 {
+    public static void main(String [] args){
+        
+        for(int i = 1; i <= 5; i++){
+            boolean flag = true;
+            for(int j = 1; j <= 9; j++){
+                if(j>=6-i && j<= 4+i && flag){
+                    System.out.print("*");
+                    flag = false;
+                }else{
+                    System.out.print(" ");
+                    flag = true;
+                }
+            }
+            System.out.println();
+        }   
+    }
+}
---
+++ Pattern_8.java
@@ -0,0 +1,26 @@
+
+public class Pattern_8 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            char ch = 'A';
+            for (int j = 1; j <= 7; j++) {
+                if (j <= 5 - i || j >= 3 + i) {
+                    System.out.print(ch);
+                        if (j < 4) {
+                            ch++;
+                        } else {
+                            ch--;
+                        }
+                } else {
+                    System.out.print(" ");
+                        if (j < 4) {
+                            ch++;
+                        } else {
+                            ch--;
+                        }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
